085105d85d612f1e2902295cfc032d7588feb811
Logan Hartford <logan.hartford@outlook.com>
2024-01-01 13:09:29 -0500

    force dark mode

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,6 +1,6 @@
 const STORAGE_KEY = "theme";
-const THEME_ATTR  = "data-theme";
-const QUERY_KEY   = "(prefers-color-scheme: dark)";
+const THEME_ATTR = "data-theme";
+const QUERY_KEY = "(prefers-color-scheme: dark)";
 
 const themes = {
   LIGHT: "light",
@@ -12,22 +12,25 @@ initTheme();
 function initTheme() {
   const savedTheme = localStorage.getItem(STORAGE_KEY);
 
-  if (savedTheme) {
-    // Storage theme
-    setTheme(savedTheme);
-  } else if (window.matchMedia && window.matchMedia(QUERY_KEY).matches) {
-    // system theme
-    setTheme(themes.DARK);
-  } else {
-    // Default theme
-    setTheme(themes.LIGHT);
-  }
+  // if (savedTheme) {
+  //   // Storage theme
+  //   setTheme(savedTheme);
+  // } else if (window.matchMedia && window.matchMedia(QUERY_KEY).matches) {
+  //   // system theme
+  //   setTheme(themes.DARK);
+  // } else {
+  //   // Default theme
+  //   setTheme(themes.LIGHT);
+  // }
 
-  // Watch for system theme changes
-  window.matchMedia(QUERY_KEY).addEventListener("change", (e) => {
-    const newTheme = e.matches ? themes.DARK : themes.LIGHT;
-    setTheme(newTheme);
-  });
+  // // Watch for system theme changes
+  // window.matchMedia(QUERY_KEY).addEventListener("change", (e) => {
+  //   const newTheme = e.matches ? themes.DARK : themes.LIGHT;
+  //   setTheme(newTheme);
+  // });
+
+  // Force dark mode
+  setTheme(themes.DARK)
 }
 
 function toggleTheme() {
